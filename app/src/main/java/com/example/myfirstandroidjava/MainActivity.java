d4b29ba6658990a17c313976530e79c4d83932dd
Takayuki Inohiza <takayuki_0711sdx@yahoo.co.jp>
2019-05-01 23:36:48 +0900

    サンプルアプリ作成

---
+++ app/src/main/java/com/example/myfirstandroidjava/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myfirstandroidjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.editText);
+                String text = editText.getText().toString();
+
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(text);
+            }
+        });
+    }
+}
